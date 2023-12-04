c9ab7fbc42892910503bb61cb507bbe87b8645f1
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-03 22:59:41 -0800

    built basic resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
             </figure>
             <h2>About Me</h2>
             <div>
-                <p>My name is Evan Griggs and I am soon to be a graduate of UCR Extension's Coding Bootcamp. As a full-stack web developer, I continue to hone my skills in
+                <p>My name is Evan Griggs and I am a graduate of UCR Extension's Coding Bootcamp. As a full-stack web developer, I am proficient in
                     HTML, CSS, JavaScript, React, SQL, and NoSQL Databases. Thank you for joining me along this journey. I look forward to sharing many more exciting
                     projects to come!</p>
             </div>
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,54 @@
+export default function Resume() {
+    return (
+        <>
+            <h1>Resume</h1>
+            <section>
+                <h2>Professional Summary</h2>
+                <p>
+                    I am a highly motivated and hardworking individual seeking to further my
+                    growth. As an aspiring web developer, I have a comprehensive understanding 
+                    of html, css, Javascript, React, MERN, SQL, and NoSQL databases. I am an 
+                    organized and professional worker that excels in building strong, positive 
+                    relationships while efficiently completing my duties.
+                    My goal is to strengthen my skills and maximize the success of my employer
+                    and my peers while pursuing my own personal and professional development.
+                </p>
+            </section>
+            <section>
+                <h2>Experience</h2>
+                <div>
+                    <h3>
+                        University of Arizona Global Campus, Arizona - Academic Advisor III
+                    </h3>
+                    <ul>
+                        <li>
+                        Provide daily support for fellow advisors immediate, on the floor 
+                        de-escalation support and mentorship
+                        </li>
+                        <li>
+                        Support the team as SME resource expert for population rules and 
+                        standard methodologies
+                        </li>
+                        <li>
+                        Use a variety of spreadsheet, word processing, database, and presentation software.
+                        </li>
+                        <li>
+                        Assist in developing key performance indicators to measure program and staff performance
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <section>
+                <h2>Education</h2>
+                <div>
+                    <h3>California State University, San Marcos </h3>
+                    <p>
+                    Bachelor of Science in Biological Sciences  
+                    </p>
+                    <h3>University of Californa: Riverside Extension</h3>
+                    <p>Fullstack Coding Bootcamp Certificate</p>
+                </div>
+            </section>
+        </>
+    );
+}
