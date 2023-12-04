1364484218727ff5df6e5ebe42c7c707c44cebd0
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-03 22:42:58 -0800

    built out About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <>
+            <figure>
+                <img src="src/assets/images" alt="handsome, happy web developer" />
+            </figure>
+            <h2>About Me</h2>
+            <div>
+                <p>My name is Evan Griggs and I am soon to be a graduate of UCR Extension's Coding Bootcamp. As a full-stack web developer, I continue to hone my skills in
+                    HTML, CSS, JavaScript, React, SQL, and NoSQL Databases. Thank you for joining me along this journey. I look forward to sharing many more exciting
+                    projects to come!</p>
+            </div>
+        </>
+    );
+}
