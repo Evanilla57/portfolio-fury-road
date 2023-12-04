ed02d4ff3935c2e1298e0a43487c120c785a75b1
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-03 22:30:14 -0800

    built out Project component, added image for github site and additional styling

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,5 +1,29 @@
 export default function Project(props) {
     const projects = props.data;
     // Maps through data from props passed through the portfolio page and to render a separate <div> for each.
-    return //TODO: Complete Project section
-};
+    return (
+        <>
+          <section>
+            {projects.map((project) => (
+              <div key={project.id}>
+                <h3>{project.title}</h3>
+                <a href={project.href}>
+                  <img
+                    src={project.src}
+                    alt={project.alt}
+                    className='project-img'
+                  />
+                </a>
+                <a href={project.repo}>
+                  <img
+                    src='src/assets/images/GitHub-logo.png'
+                    alt='github logo'
+                    className='git-hub'
+                  />
+                </a>
+              </div>
+            ))}
+          </section>
+        </>
+      );
+    };
